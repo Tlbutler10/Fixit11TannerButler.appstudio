95fb966bd1122b6e2f3272ffe3a720e66eeb36fd
Tanner <tannerbutler15@gmail.com>
2019-11-03 20:58:21 -0600

    Working thorugh second to last form

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,18 +1,23 @@
-
-
-
-
-
-
 rdoPersonType.onchange=function(){
-
-    lblDescribe.value = `I would agree that you are a ${rdoPersonType.value} person too!`
-        /* add code here to get item user chose and output 
+  switch (rdoPersonType.value){
+  case 0:
+    lblDescribe.value = `I would agree that you are a Optimistic person too!`;
+   break;     
+   case 1:
+    lblDescribe.value = `I would agree that you are a Pessimistic person too!`;
+   break;        
+  case 2:
+    lblDescribe.value = `I would agree that you are a Trusitng person too!`;
+   break;   
+   case 3:
+    lblDescribe.value = `I would agree that you are a Envious person too!`;
+   break;     
+      /* add code here to get item user chose and output 
         it in a label, using a literal.
         */
     }
 
-
+}
 btnDescribe.onclick=function(){
   ChangeForm(favExercises)
 }
--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -2,7 +2,13 @@ var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
-
+selExercises.clear() 
+    for(i=0;i<exercises.length; i++){
+    selExercises.addItem(exercises[i])
+    }
+    for(i=0;i<coreExercises.length; i++){
+    selExercises.addItem(coreExercises[i])
+    }
 }
 
 selExercises.onfocusout=function(){
