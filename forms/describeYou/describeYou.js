b65ec95a1a2e3a394cf317dc65e7af3428467a99
Tanner <tannerbutler15@gmail.com>
2019-11-03 20:46:15 -0600

    Commit

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -3,3 +3,16 @@
 
 
 
+
+rdoPersonType.onchange=function(){
+
+    lblDescribe.value = `I would agree that you are a ${rdoPersonType.value} person too!`
+        /* add code here to get item user chose and output 
+        it in a label, using a literal.
+        */
+    }
+
+
+btnDescribe.onclick=function(){
+  ChangeForm(favExercises)
+}
